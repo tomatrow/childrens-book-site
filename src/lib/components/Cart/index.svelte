520f05ddec4ff23437e326aaf9da941cac30cfc3
AJ Caldwell <tomatrow@gmail.com>
2021-11-26 19:29:14 -0800

    Remove home button from cart

--- src/lib/components/Cart/index.svelte
+++ src/lib/components/Cart/index.svelte
@@ -2,7 +2,6 @@
     import type { MetaProduct } from "$lib/types/models"
     import type { LineItem } from "$lib/common/cart"
     import { cart } from "$lib/common/cart"
-    import Link from "$lib/components/Link.svelte"
     import Button from "$lib/components/Button.svelte"
     import { fly, fade} from "svelte/transition"
     import { build } from "optional-default-site-kit/functions/build"
@@ -20,7 +19,7 @@
 </script>
 
 {#if visible}
-    <div on:click={() => (visible = false)} transition:fade class="fixed z-[100] inset-0 bg-white bg-opacity-70" />
+    <div on:click={() => (visible = false)} transition:fade class="fixed z-[225] inset-0 bg-white bg-opacity-70" />
     <section transition:fly={{ x: 400 }} class="fixed bg-white z-[250] flex flex-col max-w-md w-full top-0 right-0 bottom-0 divide-y p-8 border">
         <div class="flex justify-between py-4">
             <span class="font-semibold text-4xl">Cart</span>
@@ -42,7 +41,6 @@
         {:else}
             <div class="flex flex-col gap-4 py-8">
                 <span>No items in cart.</span>
-                <Link hollow="primary" href="/">Home</Link>
             </div>
         {/if}
     </section>
