534cabac5c4109970d5e49cfeab3404f4a941d06
AJ Caldwell <tomatrow@gmail.com>
2021-11-26 19:00:53 -0800

    Use a valid pnpm builder

--- .netlify/netlify-plugin-pnpm/index.js
+++ .netlify/netlify-plugin-pnpm/index.js
@@ -2,8 +2,8 @@ module.exports = {
     onPreBuild: async ({ utils: { build, run } }) => {
         try {
             await run.command("npm install -g pnpm")
-            await run.command("pnpm -w install")
-            await run.command("pnpm -w build")
+            await run.command("pnpm install --shamefully-hoist")
+            await run.command("pnpm build")
         } catch (error) {
             return build.failBuild(error)
         }
