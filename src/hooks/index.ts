d44d05528c4213e25d16dc4462f6b462113002d3
AJ Caldwell <tomatrow@gmail.com>
2022-01-17 21:21:13 -0800

    remove performance

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,28 +1,28 @@
 import type { Handle } from "@sveltejs/kit"
 import { sequence } from "@sveltejs/kit/hooks"
 import type { Locals } from "./index.type"
-import {
-    formatRequest,
-    formatResponse
-} from "optional-default-site-kit/functions/formatRequestResponse"
+// import {
+//     formatRequest,
+//     formatResponse
+// } from "optional-default-site-kit/functions/formatRequestResponse"
 import { installFetch } from "optional-default-site-kit/functions/request"
 
 installFetch(fetch)
 
 const handleLogging: Handle = async ({ request, resolve }) => {
-    const start = performance.now()
+    // const start = performance.now()
     const response = await resolve(request)
-    const elapsed = performance.now() - start
-    const time = (elapsed < 0.001 ? 0 : elapsed).toPrecision(3)
+    // const elapsed = performance.now() - start
+    // const time = (elapsed < 0.001 ? 0 : elapsed).toPrecision(3)
 
-    const headers = ["content-type"]
+    // const headers = ["content-type"]
 
-    console.log(
-        `[log] ${formatRequest({ request, headers })} ${formatResponse({
-            response,
-            headers
-        })} ${time}ms`
-    )
+    // console.log(
+    //     `[log] ${formatRequest({ request, headers })} ${formatResponse({
+    //         response,
+    //         headers
+    //     })} ${time}ms`
+    // )
 
     return response
 }
